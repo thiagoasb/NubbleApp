0ce2aed772054b271b88995616010a369835dae4
Thiago de Araújo <thiagoasbezerra@gmail.com>
2023-11-15 18:04:15 -0300

    chore: add Box component to limiter the Button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,6 +3,7 @@ import {useTheme} from '@shopify/restyle';
 import {TouchableOpacity} from 'react-native';
 import {Text} from '../Text/Text';
 import {Theme} from '../../theme/theme';
+import {Box} from '../Box/Box';
 
 interface ButtonProps {
   title: string;
@@ -12,17 +13,20 @@ export function Button({title}: ButtonProps) {
   const {colors} = useTheme<Theme>();
 
   return (
-    <TouchableOpacity
-      style={{
-        paddingHorizontal: 20,
-        paddingVertical: 14,
-        backgroundColor: '#074C4E',
-        alignItems: 'center',
-        borderRadius: 16,
-      }}>
+    <Box
+      backgroundColor="buttonPrimary"
+      paddingHorizontal="s20"
+      // style={{
+      //   paddingHorizontal: 20,
+      //   paddingVertical: 14,
+      //   backgroundColor: '#074C4E',
+      //   alignItems: 'center',
+      //   borderRadius: 16,
+      // }}>
+    >
       <Text preset="paragraphMedium" bold style={{color: '#fff'}}>
         {title}
       </Text>
-    </TouchableOpacity>
+    </Box>
   );
 }
