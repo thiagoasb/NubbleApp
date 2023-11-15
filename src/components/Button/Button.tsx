e1f7722670649cfe59403c86c194fbc3a9588ed4
Thiago de Araújo <thiagoasbezerra@gmail.com>
2023-11-15 18:09:41 -0300

    style: add loading and remove padding to the height fixing the style problem of the component growing bigger when the loading is active

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,21 +1,22 @@
 import React from 'react';
-import {useTheme} from '@shopify/restyle';
-import {TouchableOpacity} from 'react-native';
+import {ActivityIndicator, TouchableOpacity} from 'react-native';
 import {Text} from '../Text/Text';
 import {Theme} from '../../theme/theme';
 import {Box} from '../Box/Box';
 
 interface ButtonProps {
   title: string;
+  loading?: boolean;
 }
 
-export function Button({title}: ButtonProps) {
-  const {colors} = useTheme<Theme>();
-
+export function Button({title, loading}: ButtonProps) {
   return (
     <Box
       backgroundColor="buttonPrimary"
       paddingHorizontal="s20"
+      height={50}
+      alignItems="center"
+      justifyContent="center"
       // style={{
       //   paddingHorizontal: 20,
       //   paddingVertical: 14,
@@ -24,9 +25,13 @@ export function Button({title}: ButtonProps) {
       //   borderRadius: 16,
       // }}>
     >
-      <Text preset="paragraphMedium" bold style={{color: '#fff'}}>
-        {title}
-      </Text>
+      {loading ? (
+        <ActivityIndicator />
+      ) : (
+        <Text preset="paragraphMedium" bold style={{color: '#fff'}}>
+          {title}
+        </Text>
+      )}
     </Box>
   );
 }
