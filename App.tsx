987c6f1398a2cbb34f3b333b7b42f6897eb0b5cd
Thiago de Araújo <thiagoasbezerra@gmail.com>
2023-12-06 20:21:54 -0300

    style: add icons of stylesheet

--- App.tsx
+++ App.tsx
@@ -10,9 +10,8 @@ import {ThemeProvider} from '@shopify/restyle/dist/context';
 import {SafeAreaView, View} from 'react-native';
 import {Text} from './src/components/Text/Text';
 import {theme} from './src/theme/theme';
-import {EyeOffIcon} from './src/assets/icons/EyeOffIcon';
-import {EyeOnIcon} from './src/assets/icons/EyeOnIcon';
 import {Icon} from './src/components/Icon/Icon';
+import {Box} from './src/components/Box/Box';
 
 function App(): JSX.Element {
   return (
@@ -22,9 +21,22 @@ function App(): JSX.Element {
           <Text bold preset="headingLarge" italic>
             Coffstack
           </Text>
-          <EyeOffIcon color="blue" size={100} />
-          <EyeOnIcon size={100} color="red" />
-          <Icon name="eyeOn" color="carrotSecondary" size={50} />
+          <Box flexDirection="row">
+            <Icon name="chevronRight" size={50} />
+            <Icon name="heartFill" color="buttonPrimary" />
+            <Icon name="profile" size={50} />
+            <Icon name="profileFill" size={50} />
+            <Icon name="heart" size={50} />
+            <Icon name="bellOn" color="carrotSecondary" size={50} />
+          </Box>
+          <Box flexDirection="row">
+            <Icon name="newPost" size={50} />
+            <Icon name="camera" size={50} />
+            <Icon name="chat" size={50} />
+            <Icon name="chatOn" color="error" size={50} />
+            <Icon name="flashOff" size={50} />
+            <Icon name="flashOn" size={50} />
+          </Box>
         </View>
       </SafeAreaView>
     </ThemeProvider>
