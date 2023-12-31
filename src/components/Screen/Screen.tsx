42f9817781c381f8d4670c428b6100605bd1dd20
Thiago de Araújo <thiagoasbezerra@gmail.com>
2023-12-31 16:26:16 -0300

    feat: add SignUpScreen

--- src/components/Screen/Screen.tsx
+++ src/components/Screen/Screen.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import {Box} from '../Box/Box';
 import {useAppSafeArea} from '../../hooks/useAppSafeArea';
+import {Icon} from '../Icon/Icon';
+import {Text} from '../Text/Text';
 
 interface ScreenProps {
   children: React.ReactNode;
+  canGoBack?: boolean;
 }
 
-export function Screen({children}: ScreenProps) {
+export function Screen({children, canGoBack = false}: ScreenProps) {
   const {top} = useAppSafeArea();
 
   return (
     <Box paddingHorizontal="s24" style={{paddingTop: top}}>
+      {canGoBack && (
+        <Box mb="s24" flexDirection="row">
+          <Icon name="arrowLeft" color="primary" />
+          <Text preset="paragraphMedium" semiBold marginLeft="s8">
+            Voltar
+          </Text>
+        </Box>
+      )}
       {children}
     </Box>
   );
---
+++ src/screens/auth/SignUpScreen/SignUpScreen.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {Screen} from '../../../components/Screen/Screen';
+import {Text} from '../../../components/Text/Text';
+import {TextInput} from '../../../components/TextInput/TextInput';
+import {Icon} from '../../../components/Icon/Icon';
+import {Button} from '../../../components/Button/Button';
+
+export function SignUpScreen() {
+  function submitForm() {
+    //TODO
+  }
+
+  return (
+    <Screen canGoBack>
+      <Text preset="headingLarge" mb="s32">
+        Criar uma conta
+      </Text>
+
+      <TextInput placeholder="@" label="Seu username" boxProps={{mb: 's20'}} />
+      <TextInput
+        placeholder="Digite seu nome"
+        label="Nome Completo"
+        boxProps={{mb: 's20'}}
+      />
+      <TextInput
+        placeholder="DIgite seu e-mail"
+        label="E-mail"
+        boxProps={{mb: 's20'}}
+      />
+      <TextInput
+        label="Senha"
+        placeholder="Digite sua senha"
+        RightComponent={<Icon name="eyeOn" />}
+        boxProps={{mb: 's48'}}
+      />
+      <Button title="Criar uma conta" onPress={submitForm} />
+    </Screen>
+  );
+}
