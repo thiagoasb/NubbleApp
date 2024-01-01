37b40c014798741ddac2a1d4403564bc4daf0970
Thiago de Araújo <thiagoasbezerra@gmail.com>
2024-01-01 14:03:34 -0300

    feat: setup React Navigation

--- App.tsx
+++ App.tsx
@@ -8,16 +8,14 @@
 import React from 'react';
 import {ThemeProvider} from '@shopify/restyle/dist/context';
 import {theme} from './src/theme/theme';
-import {LoginScreen} from './src/screens/auth/LoginScreen/LoginScreen';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-import {SignUpScreen} from './src/screens/auth/SignUpScreen/SignUpScreen';
+import {Router} from './src/Routes/Router';
 
 function App(): JSX.Element {
   return (
     <SafeAreaProvider>
       <ThemeProvider theme={theme}>
-        {/* <LoginScreen /> */}
-        <SignUpScreen />
+        <Router />
       </ThemeProvider>
     </SafeAreaProvider>
   );
