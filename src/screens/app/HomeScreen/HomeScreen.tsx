8b4a9b70d23c30b5ba6f5a97d9f97c654d2120b8
Thiago de Araújo <thiagoasbezerra@gmail.com>
2024-01-07 12:49:20 -0300

    type: using generics to refact type of screens

--- src/screens/app/HomeScreen/HomeScreen.tsx
+++ src/screens/app/HomeScreen/HomeScreen.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 
-import {NativeStackScreenProps} from '@react-navigation/native-stack';
-import {AppStackParamList} from 'src/Routes/AppStack';
+import {AppScreenProps} from 'src/Routes/navigationType';
 
 import {Button, Screen, Text} from '@components';
 
-type ScreenProps = NativeStackScreenProps<AppStackParamList, 'HomeScreen'>;
-
-export function HomeScreen({navigation}: ScreenProps) {
+export function HomeScreen({navigation}: AppScreenProps<'HomeScreen'>) {
   return (
     <Screen>
       <Text preset="headingLarge">Home Screen</Text>
--- src/screens/app/SettingsScreen/SettingsScreen.tsx
+++ src/screens/app/SettingsScreen/SettingsScreen.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 
-import {NativeStackScreenProps} from '@react-navigation/native-stack';
-import {AppStackParamList} from 'src/Routes/AppStack';
+import {AppScreenProps} from 'src/Routes/navigationType';
 
 import {Screen, Text} from '@components';
 
-type ScreenProps = NativeStackScreenProps<AppStackParamList, 'SettingsScreen'>;
-
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export function SettingsScreen(props: ScreenProps) {
+export function SettingsScreen(props: AppScreenProps<'SettingsScreen'>) {
   return (
     <Screen canGoBack>
       <Text preset="headingSmall">Settings Screen</Text>
