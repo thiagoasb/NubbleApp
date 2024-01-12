cdf56986af2a5913095e915dadc1ba9fae115274
Thiago de Araújo <thiagoasbezerra@gmail.com>
2024-01-11 21:49:08 -0300

    feat: customizing bootom tab

--- src/Routes/AppTabNavigator.tsx
+++ src/Routes/AppTabNavigator.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {
+  BottomTabBarProps,
+  createBottomTabNavigator,
+} from '@react-navigation/bottom-tabs';
 
-import {Icon, Text} from '@components';
 import {
   FavoriteScreen,
   HomeScreen,
   MyProfileScreen,
   NewPostScreen,
 } from '@screens';
+
+import {AppTabBar} from './AppTabBar';
 
 export type AppTabBottomTabParamList = {
   HomeScreen: undefined;
@@ -20,31 +24,17 @@ export type AppTabBottomTabParamList = {
 const Tab = createBottomTabNavigator<AppTabBottomTabParamList>();
 
 export function AppTabNavigator() {
+  function renderTabBar(props: BottomTabBarProps) {
+    return <AppTabBar {...props} />;
+  }
+
   return (
     <Tab.Navigator
+      tabBar={renderTabBar}
       screenOptions={{
         headerShown: false,
       }}>
-      <Tab.Screen
-        options={{
-          tabBarLabel: ({focused}) => (
-            <Text
-              preset="paragraphCaption"
-              semiBold
-              color={focused ? 'primary' : 'backgroundContrast'}>
-              Início
-            </Text>
-          ),
-          tabBarIcon: ({focused}) => (
-            <Icon
-              name={focused ? 'homeFill' : 'home'}
-              color={focused ? 'primary' : 'backgroundContrast'}
-            />
-          ),
-        }}
-        name="HomeScreen"
-        component={HomeScreen}
-      />
+      <Tab.Screen name="HomeScreen" component={HomeScreen} />
       <Tab.Screen name="NewPostScreen" component={NewPostScreen} />
       <Tab.Screen name="FavoriteScreen" component={FavoriteScreen} />
       <Tab.Screen name="MyProfileScreen" component={MyProfileScreen} />
