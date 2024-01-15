f4b809c6ab7e3be7ad8b1a54f6509397696f6e23
Thiago de Araújo <thiagoasbezerra@gmail.com>
2024-01-14 22:29:04 -0300

    feat: add logo component

--- src/screens/app/HomeScreen/components/HomeHeader.tsx
+++ src/screens/app/HomeScreen/components/HomeHeader.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import {SimpleLogo} from '@brand';
+
 import {Box, BoxProps, Icon} from '@components';
 import {useAppSafeArea} from '@hooks';
 
@@ -7,7 +9,7 @@ export function HomeHeader() {
   const {top} = useAppSafeArea();
   return (
     <Box {...$wrapper} style={{padding: top}}>
-      <Box backgroundColor="carrotSecondary" height={16} width={70} />
+      <SimpleLogo width={70} />
       <Box flexDirection="row">
         <Box mr="s24">
           <Icon name="search" />
