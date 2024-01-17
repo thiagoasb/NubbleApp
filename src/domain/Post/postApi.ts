0b7f178ea578b6c12bd687ac8c4a59680dde1d01
Thiago de Araújo <thiagoasbezerra@gmail.com>
2024-01-16 21:46:30 -0300

    feat: add fetch api to get the posts

--- src/domain/Post/postApi.ts
+++ src/domain/Post/postApi.ts
@@ -2,7 +2,18 @@ import {postListMock} from './postListMock';
 import {Post} from './types';
 
 async function getList(): Promise<Post[]> {
-  await new Promise(resolve => setTimeout(() => resolve(''), 1000));
+  let response = await fetch('http://localhost:3333/user/post', {
+    method: 'GET',
+    headers: {
+      Authorization:
+        'Bearer MQ.cKPYncHXGoDvV91Y5se_XpjsjgurCtrrDjOVO6w4ftlr_WuCCE4hoq7nfRRV',
+    },
+  });
+
+  let data = await response.json();
+  console.log('FETCH DATA:', data);
+
+  // await new Promise(resolve => setTimeout(() => resolve(''), 1000));
   return postListMock;
 }
 
