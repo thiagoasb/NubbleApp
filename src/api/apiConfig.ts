52fb75b5faab1e1855f564776016e8f76d47c103
Thiago de Araújo <thiagoasbezerra@gmail.com>
2024-01-22 22:44:01 -0300

    chore: add authentication token and remove timeout before calling the API

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -4,6 +4,6 @@ export const api = axios.create({
   baseURL: 'http://127.0.0.1:3333/',
   headers: {
     Authorization:
-      'Bearer MQ.cKPYncHXGoDvV91Y5se_XpjsjgurCtrrDjOVO6w4ftlr_WuCCE4hoq7nfRRV',
+      'Bearer MQ.alB56Q8s2TzFnAuWE6XApzprF-coJqezlqylegvKpsgx_DEuvpexpxfAyZP-',
   },
 });
--- src/domain/Post/postApi.ts
+++ src/domain/Post/postApi.ts
@@ -3,7 +3,6 @@ import {PageAPI, PageParams, api} from '@api';
 import {PostAPI} from './postType';
 
 async function getList(params?: PageParams): Promise<PageAPI<PostAPI>> {
-  await new Promise(resolve => setTimeout(() => resolve(''), 1000));
   const response = await api.get<PageAPI<PostAPI>>('user/post', {
     params,
   });
