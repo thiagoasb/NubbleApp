9cdc0c8a45bef1654974ccad885868d435951145
Thiago de Araújo <thiagoasbezerra@gmail.com>
2024-01-24 22:25:17 -0300

    feat: add postCommentScreen

--- src/components/PostItem/components/PostBottom.tsx
+++ src/components/PostItem/components/PostBottom.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 
 import {Post} from '@domain';
+import {useNavigation} from '@react-navigation/native';
 
 import {Box, Text} from '@components';
 
-type Props = Pick<Post, 'author' | 'text' | 'commentCount'>;
-export function PostBottom({author, text, commentCount}: Props) {
+type Props = Pick<Post, 'author' | 'text' | 'commentCount' | 'id'>;
+export function PostBottom({author, text, commentCount, id}: Props) {
+  const navigation = useNavigation();
+
   const commentText = getCommentText(commentCount);
+
+  function navigateToPostCommentScreen() {
+    navigation.navigate('PostCommentScreen', {
+      postId: id,
+    });
+  }
+
   return (
     <Box mt="s16">
       <Text preset="paragraphMedium" bold>
@@ -16,7 +26,12 @@ export function PostBottom({author, text, commentCount}: Props) {
         {text}
       </Text>
       {commentText && (
-        <Text mt="s8" preset="paragraphSmall" bold color="primary">
+        <Text
+          onPress={navigateToPostCommentScreen}
+          mt="s8"
+          preset="paragraphSmall"
+          bold
+          color="primary">
           {commentText}
         </Text>
       )}
