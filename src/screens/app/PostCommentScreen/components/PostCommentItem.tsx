bc05de514ceef055a6bd86406a036cb1c03fbf80
Thiago de Araújo <thiagoasbezerra@gmail.com>
2024-01-28 12:07:13 -0300

    fix: update text break when don't fit in the row

--- src/screens/app/PostCommentScreen/components/PostCommentItem.tsx
+++ src/screens/app/PostCommentScreen/components/PostCommentItem.tsx
@@ -12,7 +12,7 @@ export function PostCommentItem({postComment}: Props) {
   return (
     <Box flexDirection="row" alignItems="center" mb="s16">
       <ProfileAvatar imageURL={postComment.author.profileURL} />
-      <Box ml="s12">
+      <Box ml="s12" flex={1}>
         <Text preset="paragraphSmall" bold>
           {postComment.author.userName}
         </Text>
