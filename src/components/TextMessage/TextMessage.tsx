f3b322a2b7f0e4178d55baf8488bcfb3faeb98e9
Thiago de Araújo <thiagoasbezerra@gmail.com>
2024-01-28 16:44:38 -0300

    feat: add textInput to add a new comment

---
+++ src/components/TextMessage/TextMessage.tsx
@@ -0,0 +1,57 @@
+import React, {useRef} from 'react';
+import {
+  Pressable,
+  TextInput as RNTextInput,
+  TextInputProps as RNTextInputProps,
+} from 'react-native';
+
+import {useAppTheme} from '@hooks';
+
+import {Box} from '../Box/Box';
+import {Text} from '../Text/Text';
+import {$textInputStyle} from '../TextInput/TextInput';
+
+interface TextMessageProps extends RNTextInputProps {
+  onPressSend: () => void;
+}
+
+export function TextMessage({
+  onPressSend,
+  value,
+  ...rnTextInputProps
+}: TextMessageProps) {
+  const inputRef = useRef<RNTextInput>(null);
+  const {colors} = useAppTheme();
+
+  function focusInput() {
+    inputRef.current?.focus();
+  }
+
+  const sendIsDIsabled = value?.trim().length === 0;
+
+  return (
+    <Pressable onPress={focusInput}>
+      <Box
+        paddingHorizontal="s16"
+        paddingVertical="s14"
+        backgroundColor="gray5"
+        flexDirection="row"
+        justifyContent="space-between"
+        alignItems="center"
+        borderRadius="s12">
+        <RNTextInput
+          ref={inputRef}
+          value={value}
+          placeholderTextColor={colors.gray2}
+          style={[$textInputStyle, {color: colors.gray1}]}
+          {...rnTextInputProps}
+        />
+        <Pressable disabled={sendIsDIsabled} onPress={onPressSend}>
+          <Text color={sendIsDIsabled ? 'gray2' : 'primary'} bold>
+            Enviar
+          </Text>
+        </Pressable>
+      </Box>
+    </Pressable>
+  );
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -8,6 +8,6 @@ export * from './PasswordInput/PasswordInput';
 export * from './Screen/Screen';
 export * from './Text/Text';
 export * from './TextInput/TextInput';
-
 export * from './PostItem/PostItem';
 export * from './ProfileAvatar/ProfileAvatar';
+export * from './TextMessage/TextMessage';
