b28d19a9d830c48258430cbbbc67ca23a50c4327
Thiago de Araújo <thiagoasbezerra@gmail.com>
2024-01-31 20:52:43 -0300

    feat: add funcionality of remove commentary (api and service)

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -4,6 +4,6 @@ export const api = axios.create({
   baseURL: 'http://127.0.0.1:3333/',
   headers: {
     Authorization:
-      'Bearer MQ.alB56Q8s2TzFnAuWE6XApzprF-coJqezlqylegvKpsgx_DEuvpexpxfAyZP-',
+      'Bearer Mg.Tuq_FJ1Yy0xlXcowezEXSaf87EYzObogHCs3wKJdxcpG5ITOFPDunkI5suO5',
   },
 });
