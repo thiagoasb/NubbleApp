c0fc327c75088893b0f4820d2a3f63946947797b
Thiago de Araújo <thiagoasbezerra@gmail.com>
2024-02-01 23:10:56 -0300

    feat: remove comment and add usePostCommentRemove

--- src/domain/PostComment/useCases/usePostCommentRemove.ts
+++ src/domain/PostComment/useCases/usePostCommentRemove.ts
@@ -1,1 +1,10 @@
-export function usePostCommentRemove() {}
+import {MutationOptions, useMutation} from '@infra';
+
+import {postCommentService} from '../postCommentService';
+
+export function usePostCommentRemove(option?: MutationOptions<string>) {
+  return useMutation<{postCommentId: number}, string>(
+    ({postCommentId}) => postCommentService.remove(postCommentId),
+    option,
+  );
+}
