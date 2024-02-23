af6decce70f26dd3485aef1a45b85f0944145cb1
Thiago de Araújo <thiagoasbezerra@gmail.com>
2024-02-22 22:38:38 -0300

    feat: add animation when the toast is appearing or fading in screen

--- src/components/Toast/Toast.tsx
+++ src/components/Toast/Toast.tsx
@@ -1,4 +1,5 @@
-import React, {useEffect} from 'react';
+import React, {useCallback, useEffect, useRef} from 'react';
+import {Animated} from 'react-native';
 
 import {useToast, useToastService} from '@services';
 
@@ -10,15 +11,45 @@ export function Toast() {
   const toast = useToast();
   const {hideToast} = useToastService();
 
+  const fadeAnim = useRef(new Animated.Value(0.5)).current;
+
+  const runEnteringAnimation = useCallback(() => {
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 1000,
+      useNativeDriver: true,
+    }).start();
+  }, [fadeAnim]);
+
+  const runExitingAnimation = useCallback(
+    (callback: Animated.EndCallback) => {
+      Animated.timing(fadeAnim, {
+        toValue: 0,
+        duration: 1000,
+        useNativeDriver: true,
+      }).start(callback);
+    },
+    [fadeAnim],
+  );
+
   useEffect(() => {
+    if (toast) {
+      runEnteringAnimation();
+    }
+
     setTimeout(() => {
-      hideToast();
+      runExitingAnimation(hideToast);
     }, toast?.duration || DEFAULT_DURATION);
-  }, [hideToast, toast]);
+  }, [hideToast, runEnteringAnimation, runExitingAnimation, toast]);
 
   if (!toast) {
     return null;
   }
 
-  return <ToastContent toast={toast} />;
+  return (
+    <Animated.View
+      style={{position: 'absolute', alignSelf: 'center', opacity: fadeAnim}}>
+      <ToastContent toast={toast} />
+    </Animated.View>
+  );
 }
