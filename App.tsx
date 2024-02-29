efdf805851a721176f2ff945368024e520760eef
Thiago de Araújo <thiagoasbezerra@gmail.com>
2024-02-28 21:56:47 -0300

    chore: add react query to control states

--- App.tsx
+++ App.tsx
@@ -9,6 +9,7 @@ import React from 'react';
 
 import {ToastProvider} from '@services';
 import {ThemeProvider} from '@shopify/restyle/dist/context';
+import {QueryClientProvider, QueryClient} from '@tanstack/react-query';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 import {Toast} from '@components';
@@ -16,16 +17,20 @@ import {Toast} from '@components';
 import {Router} from './src/routes/Routes';
 import {theme} from './src/theme/theme';
 
+const queryClient = new QueryClient();
+
 function App(): JSX.Element {
   return (
-    <SafeAreaProvider>
-      <ThemeProvider theme={theme}>
-        <ToastProvider>
-          <Router />
-          <Toast />
-        </ToastProvider>
-      </ThemeProvider>
-    </SafeAreaProvider>
+    <QueryClientProvider client={queryClient}>
+      <SafeAreaProvider>
+        <ThemeProvider theme={theme}>
+          <ToastProvider>
+            <Router />
+            <Toast />
+          </ToastProvider>
+        </ThemeProvider>
+      </SafeAreaProvider>
+    </QueryClientProvider>
   );
 }
 
