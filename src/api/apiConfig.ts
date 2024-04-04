6ab10d5b1ee660f2804b2f5c62ae1f3347a2a5b1
Thiago de Araújo <thiagoasbezerra@gmail.com>
2024-04-04 00:10:38 -0300

    feat: config auth token in axios

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -2,8 +2,4 @@ import axios from 'axios';
 
 export const api = axios.create({
   baseURL: 'http://127.0.0.1:3333/',
-  headers: {
-    Authorization:
-      'Bearer Mw.gSG3vwVEx_7PWxcPj0FhvT21TH7F8avZwZoNgX3rz6yOscfhCyj-fQJ_q_tp',
-  },
 });
