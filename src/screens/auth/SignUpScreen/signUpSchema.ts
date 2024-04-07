f8eb3c786cee98b3d3efa00e2774bc1f12e676d5
Thiago de Araújo <thiagoasbezerra@gmail.com>
2024-04-07 12:09:52 -0300

    feat: integrating SignUp Domain and UseCase

--- src/screens/auth/SignUpScreen/signUpSchema.ts
+++ src/screens/auth/SignUpScreen/signUpSchema.ts
@@ -1,19 +1,20 @@
+import {stringUtils} from '@utils';
 import {z} from 'zod';
 
 const userNameRegex = /^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{0,29}$/gim;
 
 export const signUpSchema = z.object({
   username: z.string().regex(userNameRegex, 'Username inválido').toLowerCase(),
-  fullName: z
+  firstName: z
     .string()
     .min(5, 'Nome muito curto')
     .max(50, 'Nome muito longo')
-    .transform(value => {
-      return value
-        .split(' ')
-        .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
-        .join(' ');
-    }),
+    .transform(stringUtils.capitalizeFirstLetter),
+  lastName: z
+    .string()
+    .min(5, 'Nome muito curto')
+    .max(50, 'Nome muito longo')
+    .transform(stringUtils.capitalizeFirstLetter),
   email: z.string().email('Email inválido'),
   password: z.string().min(8, 'Senha deve ter no mínimo 8 caracteres'),
 });
