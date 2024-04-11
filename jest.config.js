40444e024801b6b594d8a0aad6e6a4387a7eabb3
Thiago de Araújo <thiagoasbezerra@gmail.com>
2024-04-11 11:40:21 -0300

    feat: custon render

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   preset: 'react-native',
   collectCoverageFrom: ['src/{components,utils}/**/*.{js,jsx,ts,tsx}'],
   setupFilesAfterEnv: ['@testing-library/react-native/extend-expect'],
+  moduleDirectories: ['node_modules', './src/test'],
   coveragePathIgnorePatterns: ['/node_modules/', 'index'],
 };
--- src/components/Button/__test__/Button.test.tsx
+++ src/components/Button/__test__/Button.test.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
 
-import {ThemeProvider} from '@shopify/restyle';
-import {render} from '@testing-library/react-native';
-
-import {theme} from '@theme';
+import {render} from 'test-utils';
 
 import {Button} from '../Button';
 
 describe('<Button />', () => {
-  test('the components rendered', () => {
-    const {debug} = render(
-      <ThemeProvider theme={theme}>
-        <Button title="button title" />
-      </ThemeProvider>,
-    );
-    debug();
+  it('the components rendered', () => {
+    render(<Button title="button title" />);
+  });
+  it('should shows loading indicator', () => {
+    render(<Button title="button title" />);
   });
 });
