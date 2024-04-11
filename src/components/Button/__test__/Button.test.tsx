519c1572346d5a05bd3de824cc65901a4f2d8a12
Thiago de Araújo <thiagoasbezerra@gmail.com>
2024-04-11 11:19:10 -0300

    test: first test - render and debug

---
+++ src/components/Button/__test__/Button.test.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import {ThemeProvider} from '@shopify/restyle';
+import {render} from '@testing-library/react-native';
+
+import {theme} from '@theme';
+
+import {Button} from '../Button';
+
+describe('<Button />', () => {
+  test('the components rendered', () => {
+    const {debug} = render(
+      <ThemeProvider theme={theme}>
+        <Button title="button title" />
+      </ThemeProvider>,
+    );
+    debug();
+  });
+});
