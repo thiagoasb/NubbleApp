9e943f4c94b6015c2bf39f754803ee40bc0f80b7
Thiago de Araújo <thiagoasbezerra@gmail.com>
2024-04-11 11:00:36 -0300

    chore: setup initial to components test

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: 'react-native',
   collectCoverageFrom: ['src/{components,utils}/**/*.{js,jsx,ts,tsx}'],
+  setupFilesAfterEnv: ['@testing-library/react-native/extend-expect'],
   coveragePathIgnorePatterns: ['/node_modules/', 'index'],
 };
