b235d13c8e6c3cfadf12fd98a707d9c0daf28fb7
Thiago de Araújo <thiagoasbezerra@gmail.com>
2024-04-11 12:34:35 -0300

    test: pattern render component

--- src/components/Button/__test__/Button.test.tsx
+++ src/components/Button/__test__/Button.test.tsx
@@ -1,22 +1,38 @@
 import React from 'react';
 
-import {render, fireEvent} from 'test-utils';
+import {render, fireEvent, screen} from 'test-utils';
 
-import {Button} from '../Button';
+import {Button, ButtonProps} from '../Button';
+
+function renderComponent(props?: Partial<ButtonProps>) {
+  render(<Button title="button title" {...props} />);
+
+  const titleElement = screen.getByText(/button title/i);
+
+  return {
+    titleElement,
+  };
+}
 
 describe('<Button />', () => {
   it('calls the oPress function when is pressed', () => {
     const mockedOnPress = jest.fn();
-
-    const {getByText} = render(
-      <Button title="button title" onPress={mockedOnPress} />,
-    );
-
-    const titleElement = getByText(/button title/i);
+    const {titleElement} = renderComponent({onPress: mockedOnPress});
 
     fireEvent.press(titleElement);
 
     expect(mockedOnPress).toHaveBeenCalled();
+  });
+  it('does not call onPress function when is disabled and it pressed', () => {
+    const mockedOnPress = jest.fn();
+    const {titleElement} = renderComponent({
+      onPress: mockedOnPress,
+      disabled: true,
+    });
+
+    fireEvent.press(titleElement);
+
+    expect(mockedOnPress).not.toHaveBeenCalled();
   });
   it('should shows loading indicator', () => {
     render(<Button title="button title" />);
