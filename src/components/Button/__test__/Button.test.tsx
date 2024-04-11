7c53cd9c4863ce796e65a7e210e273a671c4f0b4
Thiago de Araújo <thiagoasbezerra@gmail.com>
2024-04-11 12:19:33 -0300

    test: interacting with elements (queries and events)

--- src/components/Button/__test__/Button.test.tsx
+++ src/components/Button/__test__/Button.test.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 
-import {render} from 'test-utils';
+import {render, fireEvent} from 'test-utils';
 
 import {Button} from '../Button';
 
 describe('<Button />', () => {
-  it('the components rendered', () => {
-    render(<Button title="button title" />);
+  it('calls the oPress function when is pressed', () => {
+    const mockedOnPress = jest.fn();
+
+    const {getByText} = render(
+      <Button title="button title" onPress={mockedOnPress} />,
+    );
+
+    const titleElement = getByText(/button title/i);
+
+    fireEvent.press(titleElement);
+
+    expect(mockedOnPress).toHaveBeenCalled();
   });
   it('should shows loading indicator', () => {
     render(<Button title="button title" />);
