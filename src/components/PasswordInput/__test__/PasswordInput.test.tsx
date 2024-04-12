f1f39597e6ecc67eaab1446a988ad5e5f8d529cd
Thiago de Araújo <thiagoasbezerra@gmail.com>
2024-04-12 00:01:11 -0300

    test: when pressing the eye icon, iut should show the password, and change to the eye off icon

--- src/components/PasswordInput/__test__/PasswordInput.test.tsx
+++ src/components/PasswordInput/__test__/PasswordInput.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import {render, screen} from 'test-utils';
+import {IconProps} from 'src/components/Icon/Icon';
+import {fireEvent, render, screen} from 'test-utils';
 
 import {PasswordInput} from '../PasswordInput';
 
@@ -19,5 +20,28 @@ describe('<PasswordInput />', () => {
     const inputElement = screen.getByPlaceholderText(/password/);
 
     expect(inputElement.props.secureTextEntry).toBeTruthy();
+  });
+
+  test('when pressing the eye icon, iut should show the password, and change to the eye off icon', () => {
+    const mockedOnChange = jest.fn();
+    render(
+      <PasswordInput
+        label="Password"
+        placeholder="password"
+        value="123456"
+        onChangeText={mockedOnChange}
+      />,
+    );
+
+    const eyeIcon: IconProps['name'] = 'eyeOn';
+    fireEvent.press(screen.getByTestId(eyeIcon));
+
+    const eyeOffIcon: IconProps['name'] = 'eyeOff';
+    const eyeOffIconElement = screen.getByTestId(eyeOffIcon);
+
+    expect(eyeOffIconElement).toBeTruthy();
+
+    const inputElement = screen.getByPlaceholderText(/password/);
+    expect(inputElement.props.secureTextEntry).toBeFalsy();
   });
 });
