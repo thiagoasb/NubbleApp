c611f3e4647d98c06682464c605303eb2bf2dbaa
Thiago de Araújo <thiagoasbezerra@gmail.com>
2024-04-12 23:58:50 -0300

    chore: add mockedData folder to  modulePathIgnorePatterns

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
   collectCoverageFrom: ['src/{components,utils}/**/*.{js,jsx,ts,tsx}'],
   setupFilesAfterEnv: ['@testing-library/react-native/extend-expect'],
   moduleDirectories: ['node_modules', './src/test'],
+  modulePathIgnorePatterns: ['.*/mockedData/.*'],
   coveragePathIgnorePatterns: ['/node_modules/', 'index'],
 };
