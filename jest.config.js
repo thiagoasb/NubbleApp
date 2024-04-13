8fc7951d4f26daedd0ed2a7c3aa93b45db55ee0b
Thiago de Araújo <thiagoasbezerra@gmail.com>
2024-04-13 00:14:24 -0300

    chore: add jestSetup directory

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: 'react-native',
+  setupFiles: ['<rootDir>/src/test/jestSetup.ts'],
   collectCoverageFrom: ['src/{components,utils}/**/*.{js,jsx,ts,tsx}'],
   setupFilesAfterEnv: ['@testing-library/react-native/extend-expect'],
   moduleDirectories: ['node_modules', './src/test'],
