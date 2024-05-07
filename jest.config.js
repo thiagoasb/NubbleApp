1e2a257776bc6d817035426a1e14549a5013eea6
Thiago de Araújo <thiagoasbezerra@gmail.com>
2024-05-06 22:25:58 -0300

    test: first test with render hook

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: 'react-native',
   setupFiles: ['<rootDir>/src/test/jestSetup.ts'],
-  collectCoverageFrom: ['src/{components,utils}/**/*.{js,jsx,ts,tsx}'],
+  collectCoverageFrom: ['src/{components,utils,hooks}/**/*.{js,jsx,ts,tsx}'],
   setupFilesAfterEnv: ['@testing-library/react-native/extend-expect'],
   moduleDirectories: ['node_modules', './src/test'],
   modulePathIgnorePatterns: ['.*/mockedData/.*'],
