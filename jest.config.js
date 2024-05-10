598942abb96dbd3a25c7f5c2ca22772ead30f9a8
Thiago de Araújo <thiagoasbezerra@gmail.com>
2024-05-09 22:52:25 -0300

    test: add tests to useAuthSignIn

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   preset: 'react-native',
   setupFiles: ['<rootDir>/src/test/jestSetup.ts'],
-  collectCoverageFrom: ['src/{components,utils,hooks}/**/*.{js,jsx,ts,tsx}'],
+  collectCoverageFrom: [
+    'src/{components,utils,hooks,domain}/**/*.{js,jsx,ts,tsx}',
+  ],
   setupFilesAfterEnv: ['@testing-library/react-native/extend-expect'],
   moduleDirectories: ['node_modules', './src/test'],
   modulePathIgnorePatterns: ['.*/mockedData/.*'],
--- src/test/test-utils.tsx
+++ src/test/test-utils.tsx
@@ -2,15 +2,36 @@ import React, {ReactElement} from 'react';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {ThemeProvider} from '@shopify/restyle';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {RenderOptions, render} from '@testing-library/react-native';
 
 import {theme} from '@theme';
 
+const queryClient = new QueryClient({
+  logger: {
+    log: console.log,
+    warn: console.warn,
+    error: process.env.NODE_ENV === 'test' ? () => {} : console.error,
+  },
+  defaultOptions: {
+    queries: {
+      retry: false,
+      cacheTime: Infinity,
+    },
+    mutations: {
+      retry: false,
+      cacheTime: Infinity,
+    },
+  },
+});
+
 export const AllTheProviders = ({children}: {children: React.ReactNode}) => {
   return (
-    <ThemeProvider theme={theme}>
-      <NavigationContainer>{children}</NavigationContainer>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <NavigationContainer>{children}</NavigationContainer>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 };
 
